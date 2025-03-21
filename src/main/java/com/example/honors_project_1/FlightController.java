9c750c4b6179071ff88065b0fdc12e4352d2e6bd
TheLearningHead <ayushagrawal1811@gmail.com>
2025-03-21 23:27:06 +0530

    Add Flight Controller

---
+++ src/main/java/com/example/honors_project_1/FlightController.java
@@ -0,0 +1,32 @@
+package com.example.honors_project_1;
+
+import com.example.honors_project_1.dto.Flight;
+import com.example.honors_project_1.dto.Schedule;
+import com.example.honors_project_1.services.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/flights")
+public class FlightController {
+    @Autowired
+    private FlightService flightService;
+
+    @GetMapping()
+    public List<Flight> getAllFlights(@RequestParam(defaultValue = "asc") String sort) {
+        return flightService.getAllFlights(sort);
+    }
+
+    @GetMapping("/{id}")
+    public Flight getFlightById(@PathVariable Long id) {
+        return flightService.getOneFlight(id);
+    }
+
+    @GetMapping("/{id}/schedules")
+    public List<Schedule> getSchedule(@PathVariable Long id, @RequestParam LocalDate dates) {
+        return flightService.getSchedule(id, dates);
+    }
+}
